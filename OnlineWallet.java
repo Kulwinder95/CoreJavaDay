85b6fb7039e76a721ddb697c30caf2f00623a19b
Kulwinder95 <92986068+Kulwinder95@users.noreply.github.com>
2021-10-24 22:07:48 +0530

    Add files via upload

---
+++ OnlineWallet.java
@@ -0,0 +1,11 @@
+package jaweriyas;
+
+import java.io.IOException;
+
+public class OnlineWallet extends Options {
+	public static void main(String[] args) throws IOException {
+		Options Options = new Options();
+
+		Options.getLogin();
+	}
+}
